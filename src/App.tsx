5d86772450c701210e29c97e1e8f4c6e6311e7d0
you name <you@example.com>
2024-07-02 11:20:16 -0300

    Feat: Implementando  componente Login e componente Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,28 @@ import Home from './pages/home/Home'
 import Sobre from './pages/sobre/Sobre'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Contato from './pages/contato/Contato'
+import { AuthProvider } from './contexts/AuthContext';
+import Cadastro from './pages/cadastro/Cadastro'
+import Login from './pages/login/Login'
 
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
         <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/sobre" element={<Sobre />} />
               <Route path="/contato" element={<Contato />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+    </AuthProvider>
     </>
   )
 }
---
+++ src/pages/cadastro/Cadastro.tsx
@@ -0,0 +1,163 @@
+import { ChangeEvent, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import Cliente from "../../models/Cliente"
+import { cadastrarCliente } from "../../services/Service"
+
+function Cadastro() {
+
+    let navigate = useNavigate()
+  
+    const [confirmaSenha, setConfirmaSenha] = useState<string>("")
+  
+    const [cliente, setCliente] = useState<Cliente>({
+        id: 0,
+        cnpj:"",
+        razaoSocial: "",
+        email: "",
+        senha: "",
+        foto: ""
+    })
+  
+    const [clienteResposta, setClienteResposta] = useState<Cliente>({
+        id: 0,
+        cnpj:"",
+        razaoSocial: "",
+        email: "",
+        senha: "",
+        foto: ""
+    })
+  
+    useEffect(() => {
+      if (clienteResposta.id !== 0) {
+        back()
+      }
+    }, [clienteResposta])
+  
+    function back() {
+      navigate('/login')
+    }
+  
+    function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>) {
+      setConfirmaSenha(e.target.value)
+    }
+  
+    function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+      setCliente({
+        ...cliente,
+        [e.target.name]: e.target.value
+      })
+    }
+  
+    async function cadastrarNovoCliente(e: ChangeEvent<HTMLFormElement>) {
+      e.preventDefault()
+  
+      if (confirmaSenha === cliente.senha && cliente.senha.length >= 8) {
+  
+        try {
+          await cadastrarCliente(`/clientes/cadastrar`, cliente, setClienteResposta)
+          alert('Cliente cadastrado com sucesso')
+  
+        } catch (error) {
+          alert('Erro ao cadastrar o Cliente')
+        }
+  
+      } else {
+        alert('Dados inconsistentes. Verifique as informações de cadastro.')
+        setCliente({ ...cliente, senha: "" }) 
+        setConfirmaSenha("")                
+      }
+    }
+  
+    return (
+      <>
+        <div className="grid grid-cols-1  h-screen place-items-center font-bold">
+          <div className="fundoCadastro hidden lg:block"></div>
+          <form className='flex justify-center items-center flex-col w-2/3 gap-3' onSubmit={cadastrarNovoCliente}>
+            <h2 className='text-slate-900 text-5xl'>Cadastrar</h2>
+            <div className="flex flex-col w-full">
+              <label htmlFor="nome">Razão Social</label>
+              <input
+                type="text"
+                id="razaoSocial"
+                name="razaoSocial"
+                placeholder="Razão Social"
+                className="border-2 border-slate-700 rounded p-2"
+                value={cliente.razaoSocial} 
+                onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              />
+            </div>
+            <div className="flex flex-col w-full">
+              <label htmlFor="cnpj">Cnpj</label>
+              <input
+                type="text"
+                id="cnpj"
+                name="cnpj"
+                placeholder="Cnpj"
+                className="border-2 border-slate-700 rounded p-2"
+                value={cliente.cnpj} 
+                onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              />
+            </div>
+            <div className="flex flex-col w-full">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Email"
+                className="border-2 border-slate-700 rounded p-2"
+                value={cliente.email} 
+                onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              />
+            </div>
+            <div className="flex flex-col w-full">
+              <label htmlFor="foto">Foto</label>
+              <input
+                type="text"
+                id="foto"
+                name="foto"
+                placeholder="Foto"
+                className="border-2 border-slate-700 rounded p-2"
+                value={cliente.foto} 
+                onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              />
+            </div>
+            <div className="flex flex-col w-full">
+              <label htmlFor="senha">Senha</label>
+              <input
+                type="password"
+                id="senha"
+                name="senha"
+                placeholder="Senha"
+                className="border-2 border-slate-700 rounded p-2"
+                value={cliente.senha} 
+                onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              />
+            </div>
+            <div className="flex flex-col w-full">
+              <label htmlFor="confirmarSenha">Confirmar Senha</label>
+              <input
+                type="password"
+                id="confirmarSenha"
+                name="confirmarSenha"
+                placeholder="Confirmar Senha"
+                className="border-2 border-slate-700 rounded p-2"
+                value={confirmaSenha}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
+              />
+            </div>
+            <div className="flex justify-around w-full gap-8">
+              <button className='rounded text-white bg-red-400 hover:bg-red-700 w-1/2 py-2' onClick={back}>
+                Cancelar
+              </button>
+              <button className='rounded text-white bg-indigo-400 hover:bg-indigo-900 w-1/2 py-2' type='submit'>
+                Cadastrar
+              </button>
+            </div>
+          </form>
+        </div>
+      </>
+    )
+  }
+  
+  export default Cadastro
