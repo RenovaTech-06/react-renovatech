ed2a2b7b8491c2c1e443dad78e84d383226c21b7
you name <you@example.com>
2024-07-12 13:39:47 -0300

    atualizando

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import ModalServico from '../../components/servicos/modalServico/ModalServico';
 import ListaServicos from '../../components/servicos/listarServico/listarServico';
 
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import CardServicos from '../../components/servicos/cardServicos/CardServicos';
+import Servicos from '../../models/Servicos';
+
+
 
 function Home() {
+
+  const [servicos, setServicos] = useState<Servicos[]>([]);
     return (
         <>
         
@@ -27,9 +37,85 @@ function Home() {
     </div>
   </div>
   
+  <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+
+
+        {servicos.map((servico) => (
+          <SwiperSlide key={servico.id}>
+            <CardServicos key={servico.cliente?.razaoSocial} post={servico} />
+          </SwiperSlide>
+           
+        
+      //     {items.map((item) => (
+      //   <SwiperSlide key={item.id}>
+      //     <CardServicos title={item.title} description={item.description} />
+
+      //   </SwiperSlide>
+      )
+    )}
+      </Swiper>
+
   <ListaServicos/>
         
-        
+  <section className="bg-green-600 py-40">
+        <div className="container mx-auto px-10 lg:px-20">
+          <div className="lg:max-w-2xl lg:flex lg:flex-col lg:items-center">
+            <div className="flex gap-x-2 items-center justify-center mb-2">
+             
+              <h2 className="text-white text-[2.9rem]">Missão</h2>
+            </div>
+            <p className="text-white text-center leading-20">
+            Proporcionar soluções eficientes e personalizadas para nossos clientes, 
+          oferecendo uma gestão de serviços de alta qualidade que atenda às necessidades 
+          específicas de cada setor de atuação. Nosso compromisso é entregar resultados que 
+          impulsionem o crescimento e a inovação, sempre focados na satisfação e no sucesso dos nossos clientes.
+            </p>
+          </div>
+        </div>
+        <div className="container mx-auto px-10 lg:px-20 mt-20 flex justify-end">
+          <div className="lg:max-w-2xl lg:flex-row-reverse lg:items-en lg:col-end">
+            <div className="flex gap-x-2 items-center justify-center mb-2">
+             
+              <h2 className="text-white text-[2.9rem]">Visão</h2>
+            </div>
+            <p className="text-white text-center leading-20">
+            Ser reconhecida como líder no mercado de gestão de serviços, destacando-se pela excelência no atendimento 
+          ao cliente e pela capacidade de adaptação às demandas dinâmicas do mercado. Aspiramos a ser a referência 
+          em inovação e qualidade, criando parcerias duradouras e promovendo o desenvolvimento contínuo de nossos 
+          serviços e tecnologias.
+            </p>
+          </div>
+        </div>
+        <div className="container mx-auto px-10 lg:px-20 mt-20">
+          <div className="lg:max-w-2xl lg:flex lg:flex-col lg:items-center mb-2">
+            <div className="flex gap-x-2 items-center justify-center">
+              
+              <h2 className="text-white text-[2.9rem]">Valores</h2>
+              
+            </div>
+            <h3 > Eficiência:</h3>
+            <h3 > Inovação:</h3>
+            <h3 > Transparência:</h3>
+            <h3 >Personalização:</h3>    
+            <p className="text-white text-center leading-20">
+              Nossos princípios unem: Acesso Universal a Alimentos saudáveis,
+              educação nutricional consciente, sustentabilidade agrícola e apoio
+              a pequenos produtores. Solidariedade e equidade impulsionam nosso
+              compromisso com comunidades vulneráveis. Inovação responsável e
+              transparência sustentam nossa busca por um impacto positivo,
+              moldando um futuro saudável e sustentável para todos.
+            </p>
+          </div>
+        </div>
+      </section>    
       </>
     );
 }
