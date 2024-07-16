629923ad18ad399392efddfa2ae70295a73f1bb6
your name <you@example.com>
2024-07-16 09:41:19 -0300

    style: atualizando o perfil

--- src/components/servicos/deletarServico/deletarServico.tsx
+++ src/components/servicos/deletarServico/deletarServico.tsx
@@ -95,35 +95,6 @@ function DeletarServico() {
         </div>
       </div>
     </div>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // <div className='container w-1/3 mx-auto'>
-    //   <h1 className='text-4xl text-center my-4'>Deletar serviço</h1>
-
-    //   <p className='text-center font-semibold mb-4'>Você tem certeza de que deseja apagar o serviço a seguir?</p>
-
-    //   <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-    //     <header className='py-2 px-6 bg-indigo-600 text-white font-bold text-2xl'>Servico</header>
-    //     <div className="p-4">
-    //       <p className='text-xl h-full'>{cliente.razaoSocial}</p>
-    //       <p>{servico.descricao}</p>
-    //     </div>
-    //     <div className="flex">
-    //       <button className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2' onClick={retornar}>Não</button>
-    //       <button className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-600 flex items-center justify-center' onClick={deletarServico}>
-    //         Sim
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
 
